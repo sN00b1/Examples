de457bd0c0b05fcd4c1c2482eb9bb3d20f65a961
sN00bFloat <valent.petroff2011@yandex.ru>
2023-02-26 17:37:58 +0300

    Home work 4 task 29

---
+++ C#/hw4/ex29/Program.cs
@@ -0,0 +1,5 @@
+﻿var integers = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse);
+foreach(var n in integers)
+    Console.WriteLine(n);
