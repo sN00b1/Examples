edb71f67cb79da7e0678b139390d4a8ddf29e1db
sN00bFloat <valent.petroff2011@yandex.ru>
2023-02-26 17:44:16 +0300

    Home work 5 task 34

---
+++ C#/hw5/ex34/Program.cs
@@ -0,0 +1,8 @@
+﻿var integers = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse);
+int ans = 0;
+foreach(var n in integers)
+    if(n % 2 == 0)
+        ans++;
+Console.WriteLine(ans);
