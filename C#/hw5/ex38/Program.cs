3dc0d7fc0e418620865a266b469ad7b009586af9
sN00bFloat <valent.petroff2011@yandex.ru>
2023-02-27 06:28:01 +0300

    Home work 5 task 38

---
+++ C#/hw5/ex38/Program.cs
@@ -0,0 +1,13 @@
+﻿var integers = Console.ReadLine()
+                .Split(new[] {" "}, StringSplitOptions.RemoveEmptyEntries)
+                .Select(Int32.Parse);
+int min = int.MaxValue;
+int max = int.MinValue;
+foreach(var n in integers)
+{
+    if(min > n)
+        min = n;
+    if(max < n)
+        max = n;
+}
+Console.WriteLine(max-min);
