7aa59b9349826e50fe528bbaff9cfb1961517965
sN00bFloat <valent.petroff2011@yandex.ru>
2023-05-16 20:21:39 +0300

    stadying python

---
+++ Python/informatics/nextone.py
@@ -0,0 +1,5 @@
+a = int(input())
+a1 = a + 1
+a2 = a - 1
+print("The next number for the number " + str(a) + " is " + str(a1) + ".")
+print("The previous number for the number " + str(a) + " is " + str(a2) + ".")
